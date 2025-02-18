6b6f87735ee5b86d3916272399275cf0feff5cb5
Lona Matshingana <71089848+Lona-web558@users.noreply.github.com>
2025-02-18 09:27:09 +0200

    Add files via upload

---
+++ Pseucode, basic-calculator.py
@@ -0,0 +1,42 @@
+#Pseudocode for addition, multiplication
+#subtraction, division
+
+#begin
+
+#ENTER a integer
+#ENTER a second integer
+
+#Process
+
+#ADD FIRST integer to second integer
+#multiply first integer to second integer
+#subtract first integer from second integer
+#divide first integer with second integer
+
+#OUTPUT
+#RESULT IS sum, multiplication, subtraction, division
+
+
+#ALGORITHM
+
+integer_value1 = input("Enter a integer: ")
+num1 = int(integer_value1)
+print("You have entered value 1 : ", num1)
+
+integer_value2 = input("Enter a integer: ")
+num2 = int(integer_value2)
+print("You have entered value 2 : ", num2)
+
+
+#add
+add = num1 + num2
+print("The sum is : " , add)
+#multiply
+multiply = num1 * num2
+print("The product is : ", multiply)
+#subtract
+minus = num1 - num2
+print("The difference is : ", minus)
+#divide
+divide = num1 / num2
+print("The quotient is : ", divide)
